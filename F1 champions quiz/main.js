422a2fd120febb8da2d3e0f9ccd0116e6d205468
Rafael Cabral <72042885+rafaelcabral223@users.noreply.github.com>
2021-10-13 14:36:29 -0300

    Add files via upload

---
+++ F1 champions quiz/main.js
@@ -0,0 +1,336 @@
+function check(){
+
+	var question1 = document.quiz.question1.value; //1950
+	var question2 = document.quiz.question2.value; //1951
+	var question3 = document.quiz.question3.value; //1952
+	var question4 = document.quiz.question4.value; //1953
+	var question5 = document.quiz.question5.value; //1954
+	var question6 = document.quiz.question6.value; //1955
+	var question7 = document.quiz.question7.value; //1956
+	var question8 = document.quiz.question8.value; //1957
+	var question9 = document.quiz.question9.value; //1958
+	var question10 = document.quiz.question10.value; //1959
+	var question11 = document.quiz.question11.value; //1960
+	var question12 = document.quiz.question12.value; //1961
+	var question13 = document.quiz.question13.value; //1962
+	var question14 = document.quiz.question14.value; //1963
+	var question15 = document.quiz.question15.value; //1964
+	var question16 = document.quiz.question16.value; //1965
+	var question17 = document.quiz.question17.value; //1966
+	var question18 = document.quiz.question18.value; //1967
+	var question19 = document.quiz.question19.value; //1968
+	var question20 = document.quiz.question20.value; //1969
+	var question21 = document.quiz.question21.value; //1970
+	var question22 = document.quiz.question22.value; //1971
+	var question23 = document.quiz.question23.value; //1972
+	var question24 = document.quiz.question24.value; //1973
+	var question25 = document.quiz.question25.value; //1974
+	var question26 = document.quiz.question26.value; //1975
+	var question27 = document.quiz.question27.value; //1976
+	var question28 = document.quiz.question28.value; //1977
+	var question29 = document.quiz.question29.value; //1978
+	var question30 = document.quiz.question30.value; //1979
+	var question31 = document.quiz.question31.value; //1980
+	var question32 = document.quiz.question32.value; //1981
+	var question33 = document.quiz.question33.value; //1982
+	var question34 = document.quiz.question34.value; //1983
+	var question35 = document.quiz.question35.value; //1984
+	var question36 = document.quiz.question36.value; //1985
+	var question37 = document.quiz.question37.value; //1986
+	var question38 = document.quiz.question38.value; //1987
+	var question39 = document.quiz.question39.value; //1988
+	var question40 = document.quiz.question40.value; //1989
+	var question41 = document.quiz.question41.value; //1990
+	var question42 = document.quiz.question42.value; //1991
+	var question43 = document.quiz.question43.value; //1992
+	var question44 = document.quiz.question44.value; //1993
+	var question45 = document.quiz.question45.value; //1994
+	var question46 = document.quiz.question46.value; //1995
+	var question47 = document.quiz.question47.value; //1996
+	var question48 = document.quiz.question48.value; //1997
+	var question49 = document.quiz.question49.value; //1998
+	var question50 = document.quiz.question50.value; //1999
+	var question51 = document.quiz.question51.value; //2000
+	var question52 = document.quiz.question52.value; //2001
+	var question53 = document.quiz.question53.value; //2002
+	var question54 = document.quiz.question54.value; //2003
+	var question55 = document.quiz.question55.value; //2004
+	var question56 = document.quiz.question56.value; //2005
+	var question57 = document.quiz.question57.value; //2006
+	var question58 = document.quiz.question58.value; //2007
+	var question59 = document.quiz.question59.value; //2008
+	var question60 = document.quiz.question60.value; //2009
+	var question61 = document.quiz.question61.value; //2010
+	var question62 = document.quiz.question62.value; //2011
+	var question63 = document.quiz.question63.value; //2012
+	var question64 = document.quiz.question64.value; //2013
+	var question65 = document.quiz.question65.value; //2014
+	var question66 = document.quiz.question66.value; //2015
+	var question67 = document.quiz.question67.value; //2016
+	var question68 = document.quiz.question68.value; //2017
+	var question69 = document.quiz.question69.value; //2018
+	var question70 = document.quiz.question70.value; //2019
+	var question71 = document.quiz.question71.value; //2020
+
+
+
+	var correct = 0;
+
+
+	if (question1 == "Nino Farina") {    //1950
+		correct++;
+	}
+	if (question2 == "Juan Manuel Fangio") {    //1951
+		correct++;
+	}
+	if (question3 == "Alberto Ascari") {    //1952
+		correct++;
+	}
+	if (question4 == "Alberto Ascari") {    //1953
+		correct++;
+	}
+	if (question5 == "Juan Manuel Fangio") {    //1954
+		correct++;
+	}
+	if (question6 == "Juan Manuel Fangio") {    //1955
+		correct++;
+	}
+	if (question7 == "Juan Manuel Fangio") {    //1956
+		correct++;
+	}
+	if (question8 == "Juan Manuel Fangio") {    //1957
+		correct++;
+	}
+	if (question9 == "Mike Hawthorn") {    //1958
+		correct++;
+	}
+	if (question10 == "Jack Brabham") {    //1959
+		correct++;
+	}
+	if (question11 == "Jack Brabham") {    //1960
+		correct++;
+	}
+	if (question12 == "Phil Hill") {    //1961
+		correct++;
+	}
+	if (question13 == "Graham Hill") {    //1962
+		correct++;
+	}
+	if (question14 == "Jim Clark") {    //1963
+		correct++;
+	}
+	if (question15 == "John Surtees") {    //1964
+		correct++;
+	}
+	if (question16 == "Jim Clark") {    //1965
+		correct++;
+	}
+	if (question17 == "Jack Brabham") {    //1966
+		correct++;
+	}
+	if (question18 == "Denny Hulme") {    //1967
+		correct++;
+	}
+	if (question19 == "Graham Hill") {    //1968
+		correct++;
+	}
+	if (question20 == "Jackie Steward") {    //1969
+		correct++;
+	}
+	if (question21 == "Jochen Rindt") {    //1970
+		correct++;
+	}
+	if (question22 == "Jack Steward") {    //1971
+		correct++;
+	}
+	if (question23 == "Emerson Fittipaldi") {    //1972
+		correct++;
+	}
+	if (question24 == "Jack Steward") {    //1973
+		correct++;
+	}
+	if (question25 == "Emerson Fittipaldi") {    //1974
+		correct++;
+	}
+	if (question26 == "Nick Lauda") {    //1975
+		correct++;
+	}
+	if (question27 == "James Hunt") {    //1976
+		correct++;
+	}
+	if (question28 == "Nick Lauda") {    //1977
+		correct++;
+	}
+	if (question29 == "Mario Andretti") {    //1978
+		correct++;
+	}
+	if (question30 == "Jody Scheckter") {    //1979
+		correct++;
+	}
+	if (question31 == "Alan Jones") {    //1980
+		correct++;
+	}
+	if (question32 == "Nelson Piquet") {    //1981
+		correct++;
+	}
+	if (question33 == "Keke Rosberg") {    //1982
+		correct++;
+	}
+	if (question34 == "Nelson Piquet") {    //1983
+		correct++;
+	}
+	if (question35 == "Nick Lauda") {    //1984
+		correct++;
+	}
+	if (question36 == "Alain Prost") {    //1985
+		correct++;
+	}
+	if (question37 == "Alain Prost") {    //1986
+		correct++;
+	}
+	if (question38 == "Nelson Piquet") {    //1987
+		correct++;
+	}
+	if (question39 == "Ayrton Senna") {    //1988
+		correct++;
+	}
+	if (question40 == "Alain Prost") {    //1989
+		correct++;
+	}
+	if (question41 == "Ayrton Senna") {    //1990
+		correct++;
+	}
+	if (question42 == "Ayrton Senna") {    //1991
+		correct++;
+	}
+	if (question43 == "Nigel Mansell") {    //1992
+		correct++;
+	}
+	if (question44 == "Alain Prost") {    //1993
+		correct++;
+	}
+	if (question45 == "Michael Schumacher") {    //1994
+		correct++;
+	}
+	if (question46 == "Michael Schumacher") {    //1995
+		correct++;
+	}
+	if (question47 == "Damon Hill") {    //1996
+		correct++;
+	}
+	if (question48 == "Jaques Villeneuve") {    //1997
+		correct++;
+	}
+	if (question49 == "Mika Hakkinen") {    //1998
+		correct++;
+	}
+	if (question50 == "Mika Hakkinen") {    //1999
+		correct++;
+	}
+	if (question51 == "Michael Schumacher") {    //2000
+		correct++;
+	}
+	if (question52 == "Michael Schumacher") {    //2001
+		correct++;
+	}
+	if (question53 == "Michael Schumacher") {    //2002
+		correct++;
+	}
+	if (question54 == "Michael Schumacher") {    //2003
+		correct++;
+	}
+	if (question55 == "Michael Schumacher") {    //2004
+		correct++;
+	}
+	if (question56 == "Fernando Alonso") {    //2005
+		correct++;
+	}
+	if (question57 == "Fernando Alonso") {    //2006
+		correct++;
+	}
+	if (question58 == "Kimi Raikkonen") {    //2007
+		correct++;
+	}
+	if (question59 == "Lewis Hamilton") {    //2008
+		correct++;
+	}
+	if (question60 == "Jenson Button") {    //2009
+		correct++;
+	}
+	if (question61 == "Sebastian Vettel") {    //2010
+		correct++;
+	}
+	if (question62 == "Sebastian Vettel") {    //2011
+		correct++;
+	}
+	if (question63 == "Sebastian Vettel") {    //2012
+		correct++;
+	}
+	if (question64 == "Sebastian Vettel") {    //2013
+		correct++;
+	}
+	if (question65 == "Lewis Hamilton") {    //2014
+		correct++;
+	}
+	if (question66 == "Lewis Hamilton") {    //2015
+		correct++;
+	}
+	if (question67 == "Nico Rosberg") {    //2016
+		correct++;
+	}
+	if (question68 == "Lewis Hamilton") {    //2017
+		correct++;
+	}
+	if (question69 == "Lewis Hamilton") {    //2018
+		correct++;
+	}
+	if (question70 == "Lewis Hamilton") {    //2019
+		correct++;
+	}
+	if (question71 == "Lewis Hamilton") {    //2020
+		correct++;
+	}
+
+
+
+	var pictures = ["img/LandoF1Quiz.gif", "img/HamiltonF1Quiz.gif", "img/OconF1Quiz.gif", "img/RusAndMax.gif", "img/Checo.gif", "img/OconAndAlonso.gif", "img/CameraRBR.gif"];
+	var messages = ["terrible", "you can do better than that", "not the best", "not bad but not good", "good", "awesome!", "PERFECT!"];
+	var score;
+
+	if (correct == 0) {
+		score = 0;
+	}
+
+	if (correct >= 1 && correct <= 15) {
+		score = 1;
+	}
+
+	if (correct >= 16 && correct <= 30) {
+		score = 2;
+	}
+
+	if (correct >= 31 && correct <= 45) {
+		score = 3;
+	}
+
+	if (correct >= 46 && correct <= 60) {
+		score = 4;
+	}
+
+	if (correct >= 61 && correct <= 70) {
+		score = 5;
+	}
+
+	if (correct == 71) {
+		score = 6;
+	}
+
+	
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct";
+	document.getElementById("picture").src = pictures[score];
+	}
+	
